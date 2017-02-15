340e0de694b3154fb2bfaf5cb7ea17a3606c7c6a
sweeple1131 <rakion73@unist.ac.kr>
2017-02-15 12:58:20 +0900

    Update new_error.R
    figure 1, 3, 4, 5, 8 is in here

--- new_error.R
+++ new_error.R
@@ -5,6 +5,8 @@ library(fields)
 library(akima)
 setwd("C:\\Users\\sungil.kim\\Dropbox\\Research_idea\\figs")
 
+############ Figure 1 ###############
+
 ############ Figure 3 ###############
 ##
 x=seq(0.1,10,0.1)
@@ -34,7 +36,8 @@ abs(a-b)/a #MAPE
 ##Visualization of APE in 2D
 
 ##Figure image creation
-postscript("Figure 3 - (a).eps",width = 6.0, height = 6.0, horizontal = FALSE, onefile = FALSE, paper = "special")
+##Output folder(path) should be reassigned
+postscript("C:\\Users\\sungil.kim\\Dropbox\\Research_idea\\figs\\Figure 3 - (a).eps",width = 6.0, height = 6.0, horizontal = FALSE, onefile = FALSE, paper = "special")
 z=outer(x, y, FUN=APE)
 Z=c(z)
 X=rep(x,length(y))
@@ -52,7 +55,8 @@ image.plot(akima.li,add=FALSE,xlab="A", ylab="F")
 ##APE values on the diagonal line between (A = 0.1, F = 10) and (A = 10, F = 10)
 
 ##Figure image creation
-postscript("Figure 3 - (b).eps",width = 6.0, height = 6.0, horizontal = FALSE, onefile = FALSE, paper = "special")
+##Output folder(path) should be reassigned
+postscript("C:\\Users\\sungil.kim\\Dropbox\\Research_idea\\figs\\Figure 3 - (b).eps",width = 6.0, height = 6.0, horizontal = FALSE, onefile = FALSE, paper = "special")
 #dat=cbind(X,Y,Z)
 dat_diag=cbind(x,y,diag(z))
 #dat_diag=dat[(dat[,1]+dat[,2]==(max(x)+min(x))),]
@@ -84,7 +88,8 @@ mtext("A",1,line=3,at=-0.2)
 ##Visualization of AAPE in 2D
 
 ##Figure image creation
-postscript("Figure 3 - (c).eps",width = 6.0, height = 6.0, horizontal = FALSE, onefile = FALSE, paper = "special")
+##Output folder(path) should be reassigned
+postscript("C:\\Users\\sungil.kim\\Dropbox\\Research_idea\\figs\\Figure 3 - (c).eps",width = 6.0, height = 6.0, horizontal = FALSE, onefile = FALSE, paper = "special")
 z=outer(x, y, FUN=AAPE)
 Z=c(z)
 X=rep(x,length(y))
@@ -102,7 +107,8 @@ image.plot(akima.li,add=FALSE,xlab="A", ylab="F")
 ##AAPE values on the diagonal line between (A = 0.1, F = 10) and (A = 10, F = 0)
 
 ##Figure image creation
-postscript("Figure 3 - (b).eps",width = 6.0, height = 6.0, horizontal = FALSE, onefile = FALSE, paper = "special")
+##Output folder(path) should be reassigned
+postscript("C:\\Users\\sungil.kim\\Dropbox\\Research_idea\\figs\\Figure 3 - (d).eps",width = 6.0, height = 6.0, horizontal = FALSE, onefile = FALSE, paper = "special")
 #dat=cbind(X,Y,Z)
 dat_diag=cbind(x,y,diag(z))
 #dat_diag=dat[(dat[,1]+dat[,2]==(max(x)+min(x))),]
@@ -125,75 +131,108 @@ mtext("F",1,line=1,at=-1)
 axis(1, pretty(dat_diag[,1],10),line=3)
 mtext("A",1,line=3,at=-1)
 
+
+
+
+
+
 ############ Figure 4 ###############
+##Visualization of APE and AAPE on [1,10] x [1,10].
 
 ##Fig 4 - (a)
 ## APE
 
-##z=outer(x, y, FUN=APE)
-##Z=c(z)
-##X=rep(x,length(y))
-##Y=rep(y,each=length(x))
-##akima.li <- interp(X,Y,Z, duplicate="median")
-##image.plot(akima.li,add=FALSE,xlab="A", ylab="F")
+z=outer(x, y, FUN=APE)
+Z=c(z)
+X=rep(x,length(y))
+Y=rep(y,each=length(x))
 
-x=seq(1,10,0.1)
-y=sort(x, decreasing = TRUE)
+##Figure image creation
+##Output folder(path) should be reassigned
+postscript("C:\\Users\\sungil.kim\\Dropbox\\Research_idea\\figs\\Figure 4 - (a).eps",width = 6.0, height = 6.0, horizontal = FALSE, onefile = FALSE, paper = "special")
+#dat=cbind(X,Y,Z)
 dat_diag=cbind(x,y,diag(z))
-##dat_diag2= dat_diag[2*x>y,]
-plot(dat_diag[,1],dat_diag[,3],xlab="",ylab="APE",type="l",axes=F)
-axis(2, pretty(round(dat_diag[,3],2),10))
+#dat_diag=dat[(dat[,1]+dat[,2]==(max(x)+min(x))),]
+dat_diag2= dat_diag[9.2>y,]
+plot(dat_diag2[,1],dat_diag2[,3],xlab="",ylab="APE",type="l",axes=FALSE)
+axis(2, pretty(round(dat_diag2[,3],2),3))
 axis(1, at=1:10,labels=c(10:1),line=1)
-mtext("F",1,line=1,at=0.2)
+mtext("F",1,line=1,at=-0.2)
 
-axis(1, pretty(dat_diag2[,1],10),line=3)
-mtext("A",1,line=3,at=0.2)
+#axis(1, pretty(dat_diag[,1],10),line=3)
+axis(1, at=1:10,labels=c(1:10),line=3)
+mtext("A",1,line=3,at=-0.2)
 
-postscript("APE.eps",width = 6.0, height = 6.0, horizontal = FALSE, onefile = FALSE, paper = "special")
-
-
-##dat_diag=cbind(x,y,diag(z))
-##plot(dat_diag[,1],dat_diag2[,3],xlab="",ylab="APE",type="l",axes=FALSE)
-##axis(2, pretty(round(dat_diag2[,3],2),3))
-##axis(1, at=1:10,labels=c(10:1),line=1)
-##mtext("F",1,line=1,at=0.8)
-##	axis(1, at=1:10,labels=c(1:10),line=3)
-##	mtext("A",1,line=3,at=0.8)
 dev.off() 
+
+
+##Figure image plotting
+plot(dat_diag2[,1],dat_diag2[,3],xlab="",ylab="APE",type="l",axes=FALSE)
+axis(2, pretty(round(dat_diag2[,3],2),3))
+axis(1, at=1:10,labels=c(10:1),line=1)
+mtext("F",1,line=1,at=-0.2)
+
+#axis(1, pretty(dat_diag[,1],10),line=3)
+axis(1, at=1:10,labels=c(1:10),line=3)
+mtext("A",1,line=3,at=-0.2)
+
+
+
 
 ##Fig 4 - (b)
 ## AAPE
+
 z=outer(x, y, FUN=AAPE)
 Z=c(z)
 X=rep(x,length(y))
 Y=rep(y,each=length(x))
-akima.li <- interp(X,Y,Z, duplicate="median")
-image.plot(akima.li,add=FALSE,xlab="A", ylab="F")
+#akima.li <- interp(X,Y,Z, duplicate="median")
+#image.plot(akima.li,add=FALSE,xlab="A", ylab="F")
 
-
-postscript("AAPE.eps",width = 6.0, height = 6.0, horizontal = FALSE, onefile = FALSE, paper = "special")
+##Figure image creation
+##Output folder(path) should be reassigned
+postscript("C:\\Users\\sungil.kim\\Dropbox\\Research_idea\\figs\\Figure 4 - (b).eps",width = 6.0, height = 6.0, horizontal = FALSE, onefile = FALSE, paper = "special")
 	dat_diag=cbind(x,y,diag(z))
-	plot(dat_diag[,1],dat_diag[,3],xlab="",ylab="AAPE",type="l",axes=FALSE)
-	axis(2, pretty(round(dat_diag[,3],2),10))
-	axis(1, at=1:3,labels=c(3:1),line=1)
+	dat_diag3= dat_diag[9*x>y,]
+	plot(dat_diag3[,1],dat_diag3[,3],xlab="",ylab="AAPE",type="l",axes=FALSE)
+	axis(2, pretty(round(dat_diag3[,3],2),10))
+	axis(1, at=1:10,labels=c(10:1),line=1)
 	mtext("F",1,line=1,at=0.8)
-	axis(1, at=1:3,labels=c(1:3),line=3)
+	axis(1, at=1:10,labels=c(1:10),line=3)
 	mtext("A",1,line=3,at=0.8)
 dev.off() 
 
+##Figure image plotting
+plot(dat_diag3[,1],dat_diag3[,3],xlab="",ylab="AAPE",type="l",axes=F)
+axis(2, pretty(round(dat_diag3[,3],2),10))
+axis(1, at=0:10,labels=c(10:0),line=1)
+mtext("F",1,line=1,at=-1)
+axis(1, pretty(dat_diag[,1],10),line=3)
+mtext("A",1,line=3,at=-1)
 
 ############## M3 data ###############
 
 
-
+##Figure 1
+##Three years of monthly sales of a lubricant product sold in large containers.
 library(Mcomp)
 data(M3)
 plot(M3$N0647)
 library(fma)
-#postscript("lubricant.eps",width = 8.0, height = 3.0, horizontal = FALSE, onefile = FALSE, paper = "special")
+
+##Figure image creation
+postscript("C:\\Users\\sungil.kim\\Dropbox\\Research_idea\\figs\\Figure 1.eps",width = 8.0, height = 3.0, horizontal = FALSE, onefile = FALSE, paper = "special")
 plot(c(1:36),productC,ylab="Unit sold",type="b",xlab="Month",lwd=3)
 abline(v=24.5,lty=2,lwd=3)
-#dev.off() 
+dev.off() 
+
+##Figure image plotting
+plot(c(1:36),productC,ylab="Unit sold",type="b",xlab="Month",lwd=3)
+abline(v=24.5,lty=2,lwd=3)
+
+
+############ Figure 8 ###############
+##Actual data and two different forecasts from the 25th to 36th months
 
 productC[1:24]
 
@@ -231,7 +270,7 @@ mean(APE(productC[c(28,29,32,34)],F))
 mean(APE(productC[c(28,29,32,34)],c(6,1.1,0.9,1.1)))
 
 
-postscript("lubricant_plot.eps",width = 5.0, height = 5.0, horizontal = FALSE, onefile = FALSE, paper = "special")
+postscript("C:\\Users\\sungil.kim\\Dropbox\\Research_idea\\figs\\Figure 8.eps",width = 5.0, height = 5.0, horizontal = FALSE, onefile = FALSE, paper = "special")
 plot(c(25:36),productC[25:36],type="l",xlab="Month", ylab="Sales",lwd=3, ylim=c(-0.5,7.5))
 abline(h=F,col=4,lwd=3,lty=2)
 lines(c(25:36),c(0.1,-0.1,0.1,7,1.1,0.1,-0.1,0.9,0.1,1.1,-0.1,0.1),col=2,lwd=3,lty=3)
@@ -239,12 +278,20 @@ legend(31.5, 7, c("Actual", "Forecast 1", "Forecast 2"), col = c(1,4,2),
 lty = c(1,2,3), lwd=c(3,3,3),cex=0.9,merge = T,box.col="white")
 dev.off() 
 
+plot(c(25:36),productC[25:36],type="l",xlab="Month", ylab="Sales",lwd=3, ylim=c(-0.5,7.5))
+abline(h=F,col=4,lwd=3,lty=2)
+lines(c(25:36),c(0.1,-0.1,0.1,7,1.1,0.1,-0.1,0.9,0.1,1.1,-0.1,0.1),col=2,lwd=3,lty=3)
+legend(31.5, 7, c("Actual", "Forecast 1", "Forecast 2"), col = c(1,4,2),
+       lty = c(1,2,3), lwd=c(3,3,3),cex=0.9,merge = T,box.col="white")
+
 library(xtable)
 DD=data.frame(cbind(c(productC),APE(productC,F),AAPE(productC,F)))
 
 xtable(DD)
 
 ###################################
+############ Figure 5 ###############
+
 
 x=seq(0,6,0.1)
 y=atan(x)
@@ -253,7 +300,7 @@ y=atan(x)
 x2=seq(1.5,6,0.1)
 y2=pi/2-1/x2
 
-postscript("arctan.eps",width = 6.0, height = 6.0, horizontal = FALSE, onefile = FALSE, paper = "special")
+postscript("C:\\Users\\sungil.kim\\Dropbox\\Research_idea\\figs\\Figure 5.eps",width = 6.0, height = 6.0, horizontal = FALSE, onefile = FALSE, paper = "special")
 
 plot(x,y,type="l",ylab="y",lwd=2,ylim=c(0,2))
 lines(x,x,lwd=2,lty=2)
