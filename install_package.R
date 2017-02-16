f7e9ec8ab58814a698a546854cda879626230727
sweeple1131 <rakion73@unist.ac.kr>
2017-02-17 04:40:49 +0900

    Add files via upload

---
+++ install_package.R
@@ -0,0 +1,8 @@
+##Installing package
+install.packages("Mcomp", repos = "http://cran.us.r-project.org")
+install.packages("fma", repos = "http://cran.us.r-project.org")
+install.packages("fields", repos = "http://cran.us.r-project.org")
+install.packages("akima", repos = "http://cran.us.r-project.org")
+install.packages("plyr", repos = "http://cran.us.r-project.org")
+install.packages("forecast", repos = "http://cran.us.r-project.org")
+install.packages("xtable", repos = "http://cran.us.r-project.org")
